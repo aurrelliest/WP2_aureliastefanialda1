e76bd6f11977679f14d50d71de12c66b636c1043
aurrelliest <aureliastefani180@gmail.com>
2022-09-18 04:00:10 +0700

    menambahkan

---
+++ application/controllers/contoh.php
@@ -0,0 +1,11 @@
+<?php
+class contoh extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Aurelia Stefani alda
+              saya tinggal di Desa Madusari
+              saya suka nonton drama korea";
+    }
+}
